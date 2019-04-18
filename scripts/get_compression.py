e12dc2527dde93e01dcdaa80d8823d18937c42be
George Bissias <bissias@gmail.com>
2019-04-18 07:48:24 -0400

    interim report: incorporate feedback; add raw compression data

--- scripts/get_compression.py
+++ scripts/get_compression.py
@@ -4,6 +4,7 @@ import pylab as plt
 import re
 
 log_file = input('Log file path: ')
+out_file = input('Out file path: ')
 
 with open(log_file) as fd:
     lines = fd.readlines()
@@ -17,32 +18,38 @@ with open(log_file) as fd:
             res = re.match('.*?Acceptable block.*? Tx:([\d]+).*?', lines[i+2])
             if res is not None:
                 sizes.append(int(res.group(1)))
-    large_size_ratios = [pair[1] for pair in zip(sizes, ratios) if pair[0] > 1000]
-    print('best compression overall: ', max(ratios))
-    print()
-    print('Results for all blocks, n: ', len(ratios))
-    print('mean compression: ', np.mean(ratios))
-    print('median compression: ', np.median(ratios))
-    print()
-    print('Results for blocks with more than 1K txs, n: ', len(large_size_ratios))
-    print('mean compression: ', np.mean(large_size_ratios))
-    print('median compression: ', np.median(large_size_ratios))
-    plt.scatter(sizes, ratios, alpha=0.75)
-    plt.xlabel('transactions in block')
-    plt.ylabel('compression ratio')
-    plt.ylim((0.95, 1.0))
-    plt.grid(True)
 
-    size_ratios_map = defaultdict(list)
+with open(out_file, 'w') as fd:
+    fd.write('block_txs,compression_ratio\n') 
     for size, ratio in zip(sizes, ratios):
-        size_ratios_map[size//50].append(ratio)
+        fd.write('%d,%f\n' % (size, ratio))
 
-    plt.figure()
-    plt.boxplot(size_ratios_map.values())
-    plt.xlabel('transactions in block')
-    plt.ylabel('compression ratio')
-    plt.ylim((0.95, 1.0))
-    plt.xticks(rotation='vertical')
-    plt.xticks(sorted(size_ratios_map.keys()), [50*k for k in sorted(size_ratios_map.keys())], rotation='vertical')
-    plt.tight_layout()
-    plt.show()
+large_size_ratios = [pair[1] for pair in zip(sizes, ratios) if pair[0] > 1000]
+print('best compression overall: ', max(ratios))
+print()
+print('Results for all blocks, n: ', len(ratios))
+print('mean compression: ', np.mean(ratios))
+print('median compression: ', np.median(ratios))
+print()
+print('Results for blocks with more than 1K txs, n: ', len(large_size_ratios))
+print('mean compression: ', np.mean(large_size_ratios))
+print('median compression: ', np.median(large_size_ratios))
+plt.scatter(sizes, ratios, alpha=0.75)
+plt.xlabel('transactions in block')
+plt.ylabel('compression ratio')
+plt.ylim((0.95, 1.0))
+plt.grid(True)
+
+size_ratios_map = defaultdict(list)
+for size, ratio in zip(sizes, ratios):
+    size_ratios_map[size//50].append(ratio)
+
+plt.figure()
+plt.boxplot(size_ratios_map.values())
+plt.xlabel('transactions in block')
+plt.ylabel('compression rate')
+plt.ylim((0.95, 1.0))
+plt.xticks(rotation='vertical')
+plt.xticks(sorted(size_ratios_map.keys()), [50*k for k in sorted(size_ratios_map.keys())], rotation='vertical')
+plt.tight_layout()
+plt.show()
